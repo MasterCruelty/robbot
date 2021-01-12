a4a8d94e4d6190ea284a965122cba18338b2d1ed
Roberto <roberto.antoniello@studenti.unimi.it>
2021-01-12 18:20:05 +0100

    added two fields in covid function

--- modules/covid.py
+++ modules/covid.py
@@ -32,10 +32,12 @@ def covid_daily(client,message):
     data = json.loads(resp.text)
     for item in data:
         nuovi_positivi = str(item["nuovi_positivi"])
+        var_positivi = str(item["variazione_totale_positivi"])
         ricoverati = str(item["ricoverati_con_sintomi"])
         terapia_intensiva = str(item["terapia_intensiva"])
+        ingressi_ti = str(item["ingressi_terapia_intensiva"])
         isolamento = str(item["isolamento_domiciliare"])
         deceduti = str(item["deceduti"])
         giorno = str(item["data"])[0:10]
-    result = "I nuovi positivi in data " + giorno +" sono: " + nuovi_positivi + "\nAttualmente vi sono:\n" + ricoverati + " pazienti ricoverati con sintomi\n" + terapia_intensiva + " pazienti in terapia intensiva\n" + isolamento + " pazienti in isolamento domiciliare\n" + deceduti + " pazienti deceduti"
+    result = "I nuovi positivi in data " + giorno +" sono: " + nuovi_positivi + "\nAttualmente vi sono:\n" + ricoverati + " pazienti ricoverati con sintomi\n" + terapia_intensiva + " pazienti in terapia intensiva\n" + isolamento + " pazienti in isolamento domiciliare\n" + deceduti + " pazienti deceduti" +"\n\n" + "ingressi t.i. : " + ingressi_ti + "\nvariazione positivi: " + var_positivi
     return utils.get_config.sendMessage(client,message,result)
