4ef11adc8ed60d952b18fb454a431dfbd2f61bed
Roberto <roberto.antoniello@studenti.unimi.it>
2021-03-14 21:06:47 +0100

    created get_today_forecasts function

--- modules/weather.py
+++ modules/weather.py
@@ -3,7 +3,7 @@ sys.path.append(sys.path[0] + "/..")
 from modules.gmaps import showmaps
 import requests
 import json
-from datetime import datetime as dt
+from datetime import date,datetime as dt
 import pytz
 from utils.get_config import *
 from pyrogram import Client
@@ -34,8 +34,8 @@ def get_weather(client,message,query):
     umidita = str(data["current"]["humidity"]) # % of umidity
     clouds = str(data["current"]["clouds"])    # % of cloudiness
     visibility = str(data["current"]["visibility"]) # Visibility in metres
-    wind_speed = data["current"]["wind_speed"] #speed in m/s
-    wind_speed = str(wind_speed * 3.6)
+    wind_speed = data["current"]["wind_speed"] * 3.6 #speed in m/s
+    wind_speed = str(round(wind_speed,2))
     weather = data["current"]["weather"][0]["description"]
     #sunrise and sunset UNIX time
     sunset = data["current"]["sunset"]
@@ -46,4 +46,27 @@ def get_weather(client,message,query):
     #Result string
     result = "**" + query.title() + "**" + "\n**Meteo:** __" + weather + "__\n**Temperatura attuale:** __" + current_temp + " C°__.\n**Temperatura percepita:** __" + feels_temp + " C°__.\n**Umidità:**__" + umidita + "%__.\n**Nuvole:** __" + clouds + "%__.\n**Visibilità:** __" + visibility + " metri__.\n**Velocità del vento:** __" + wind_speed + " km/h__.\n**Ora alba:** __" + sunrise + "__\n**Ora tramonto:** __" + sunset + "__"
     return sendMessage(client,message,result)
+
+
+"""
+    Json ottenuto in modo analogo a "get_weather" e vengono rilasciati i dati meteo principali del giorno corrente distanziati di ora in ora.
+"""
+def get_today_forecasts(client,message,query):
+    data = call_api_weather(query)
+    array_hourly = data["hourly"]
+    today = date.today()
+    today = str(today.strftime("%Y-%m-%d"))
+    result = "**" + query.title() + "** __" + str(today) + "__\n"
+    for item in array_hourly:
+        giorno = str(dt.fromtimestamp(item["dt"],pytz.timezone('Europe/Rome')))
+        if(giorno[0:10] != today):
+            break
+        giorno = giorno[10:]
+        temp = str(item["temp"]) + " C°"
+        feels_temp = str(item["feels_like"]) + " C°"
+        clouds = str(item["clouds"]) + "%"
+        weather = item["weather"][0]["description"]
+        result += giorno + "\n**Meteo:** __" + weather + "__\n**Temperatura:** __" + temp + "__\n**Temperatura percepita:** __" + feels_temp + "__\n**Nuvole:** __" + clouds + "__\n************\n\n" 
+    return sendMessage(client,message,result)
+
 
