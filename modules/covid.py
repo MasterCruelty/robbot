fccb62fcdecc4fb9d6583cb4018362c6e8883039
Roberto <roberto.antoniello@studenti.unimi.it>
2021-04-02 19:26:28 +0200

    :snake: refactoring :snake:

--- modules/covid.py
+++ modules/covid.py
@@ -4,63 +4,14 @@ import json
 import utils.get_config
 
 """
-    Funzione di supporto che decide quale funzione covid deve essere eseguita
-"""
-def execute_covid(client,message,query):
-    if(query != "/covid"):
-        return covidRegions(client,message,query)
-    else:
-        return covid_daily(client,message)
+    url => url github da cui recuperare il json
 
+    Funzione di supporto per restituire il json sui contagi da covid19
 """
-    Identica a covid_daily ma fornisce i dati della regione richiesta.
-"""
-def covidRegions(client,message,query):
-    url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni-latest.json'
+def covid_format_json(url):
     resp = requests.get(url)
     data = json.loads(resp.text)
-    trovata = False
-    for item in data:
-        if query.title()[0:4] in item["denominazione_regione"]:
-            regione = item["denominazione_regione"]
-            nuovi_positivi = str(item["nuovi_positivi"])
-            var_positivi = str(item["variazione_totale_positivi"])
-            ricoverati = str(item["ricoverati_con_sintomi"])
-            terapia_intensiva = str(item["terapia_intensiva"])
-            ingressi_ti = str(item["ingressi_terapia_intensiva"])
-            isolamento = str(item["isolamento_domiciliare"])
-            deceduti = str(item["deceduti"])
-            giorno = str(item["data"])[0:10]
-            trovata = True
-            break
-    if(trovata):
-        result = "I nuovi positivi in data **" + giorno +"** nella regione __**" + regione + "**__  sono: **" + nuovi_positivi + "**\nAttualmente vi sono:\n\n __pazienti ricoverati con sintomi:__ **" +ricoverati +"**\n __pazienti in terapia intensiva:__ **" + terapia_intensiva + "**\n __pazienti in isolamento domiciliare:__ **" + isolamento + "**\n __ pazienti deceduti:__ **" + deceduti + "**\n\n" + "__ingressi t.i. :__ **" + ingressi_ti + "**\n__variazione positivi:__ **" + var_positivi + "**"
-        return utils.get_config.sendMessage(client,message,result)
-    else:
-        return utils.get_config.sendMessage(client,message,"__Regione non trovata__")
-
-
-
-"""
-    funzione che prende ogni giorno il json aggiornato contenente i dati dei contagiati in Italia.
-    Direttamente dal repository git di salute.gov.it
-"""
-def covid_daily(client,message):
-    url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json'
-    resp = requests.get(url)
-    data = json.loads(resp.text)
-    for item in data:
-        nuovi_positivi = str(item["nuovi_positivi"])
-        var_positivi = str(item["variazione_totale_positivi"])
-        ricoverati = str(item["ricoverati_con_sintomi"])
-        terapia_intensiva = str(item["terapia_intensiva"])
-        ingressi_ti = str(item["ingressi_terapia_intensiva"])
-        isolamento = str(item["isolamento_domiciliare"])
-        deceduti = str(item["deceduti"])
-        giorno = str(item["data"])[0:10]
-    result = "I nuovi positivi in data **" + giorno +"** in **__Italia**__ sono: **" + nuovi_positivi + "**\nAttualmente vi sono:\n\n __pazienti ricoverati con sintomi:__ **" +ricoverati +"**\n __pazienti in terapia intensiva:__ **" + terapia_intensiva + "**\n __pazienti in isolamento domiciliare:__ **" + isolamento + "**\n __ pazienti deceduti:__ **" + deceduti + "**\n\n" + "__ingressi t.i. :__ **" + ingressi_ti + "**\n__variazione positivi:__ **" + var_positivi + "**"
-    return utils.get_config.sendMessage(client,message,result)
-
+    return data
 
 """
     url => url github da dove prendere il json con i dati.
@@ -83,6 +34,45 @@ def format_values(number):
     return formated
 
 """
+    Identica a covid_daily ma fornisce i dati della regione richiesta.
+"""
+def covid_cases(client,message,query):
+    regioni = covid_format_json('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni-latest.json')
+    italia  = covid_format_json('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json')
+    trovata = False
+    for item in regioni:
+        if(query.title()[0:4] in item["denominazione_regione"]):
+            regione = item["denominazione_regione"]
+            nuovi_positivi = str(item["nuovi_positivi"])
+            var_positivi = str(item["variazione_totale_positivi"])
+            ricoverati = str(item["ricoverati_con_sintomi"])
+            terapia_intensiva = str(item["terapia_intensiva"])
+            ingressi_ti = str(item["ingressi_terapia_intensiva"])
+            isolamento = str(item["isolamento_domiciliare"])
+            deceduti = str(item["deceduti"])
+            giorno = str(item["data"])[0:10]
+            trovata = True
+            break
+        elif(query == "/covid"):
+            for item in italia:
+                regione = "Italia"
+                nuovi_positivi = str(item["nuovi_positivi"])
+                var_positivi = str(item["variazione_totale_positivi"])
+                ricoverati = str(item["ricoverati_con_sintomi"])
+                terapia_intensiva = str(item["terapia_intensiva"])
+                ingressi_ti = str(item["ingressi_terapia_intensiva"])
+                isolamento = str(item["isolamento_domiciliare"])
+                deceduti = str(item["deceduti"])
+                giorno = str(item["data"])[0:10]
+                trovata = True
+            break
+    if(trovata):
+        result = "I nuovi positivi in data **" + giorno +"** in __**" + regione + "**__  sono: **" + format_values(nuovi_positivi) + "**\nAttualmente vi sono:\n\n __pazienti ricoverati con sintomi:__ **" +format_values(ricoverati) +"**\n __pazienti in terapia intensiva:__ **" + format_values(terapia_intensiva) + "**\n __pazienti in isolamento domiciliare:__ **" + format_values(isolamento) + "**\n __pazienti deceduti:__ **" + format_values(deceduti) + "**\n\n" + "__ingressi t.i. :__ **" + format_values(ingressi_ti) + "**\n__variazione positivi:__ **" + format_values(var_positivi) + "**"
+        return utils.get_config.sendMessage(client,message,result)
+    else:
+        return utils.get_config.sendMessage(client,message,"__Regione non trovata__")
+
+"""
     client,message => parametri necessari per poter usare sendMessage del modulo 'get_config'
     query => regione richiesta, di default è l'Italia intera.
 
@@ -99,11 +89,13 @@ def vaccine(client,message,query):
             total_consegne += item["dosi_consegnate"]
             total_somm += item["dosi_somministrate"]
             regione = "Italia"
-        else:
-            if(query.title()[0:4] in item["nome_area"]):
+        elif(query.title()[0:4] in item["nome_area"]):
                 total_consegne += item["dosi_consegnate"]
                 total_somm += item["dosi_somministrate"]
                 regione = item["nome_area"]
+        else:
+            return utils.get_config.sendMessage(client,message,"__Regione non trovata__")
+
     perc = str(round(((total_somm * 100) / total_consegne),2))
     giorno = str(data_total[0]["ultimo_aggiornamento"])[0:10]
     pfizer, moderna, astra = 0,0,0
