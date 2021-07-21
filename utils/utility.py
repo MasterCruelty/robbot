15fb26fd7d76639e55c45a903586c9e48588a255
Roberto <roberto.antoniello@studenti.unimi.it>
2021-07-21 19:25:32 +0200

    Remove /edatm
    Reference to #10

--- utils/utility.py
+++ utils/utility.py
@@ -26,8 +26,6 @@ def fetch_command(match,query,client,message):
         return modules.atm_feature.get_stop_info(query,client,message)
     if match == "/geoatm":
         return modules.atm_feature.geodata_stop(query,client,message)
-    if match == "/edatm":
-        return modules.atm_feature.get_rivendita_info(query,client,message)
     if match == "/searchatm":
         return modules.atm_feature.search_line(client,message,query)
     if match == "/covid":
