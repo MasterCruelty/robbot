56da6a0900c859d1f460843906ceab9f08d4af59
Roberto <roberto.antoniello@studenti.unimi.it>
2022-03-13 21:11:14 +0100

    Update ai feature

--- modules/openai.py
+++ modules/openai.py
@@ -11,13 +11,19 @@ Dato l'input, viene generato un testo randomico tramite le api di OpenAI
 """
 def openai_completion(query,client,message):
     openai.api_key = api_openai
+    stop_seq = ""
+    if "stop" in query:
+        temp = query.split("/")
+        stop_seq = temp[0].replace("stop","")
+        query = temp[1]
     resp = openai.Completion.create(
             engine="text-davinci-001",
             prompt=query,
-            temperature=0.7,
+            temperature=0.8,
             max_tokens=1024,
             top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0)
-    result = resp["choices"][0]["text"]
+            stop = stop_seq,
+            frequency_penalty=0.8,
+            presence_penalty=0.8)
+    result = "**" + query + "** " + resp["choices"][0]["text"]
     return sendMessage(client,message,result)
