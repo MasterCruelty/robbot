c962f1a3fb5508ed0fff93bddde3533c0e6f75af
Roberto <robertino.antoniello@gmail.com>
2022-05-17 18:31:35 +0200

    :wrench: formatting result

--- modules/urban.py
+++ modules/urban.py
@@ -12,6 +12,7 @@ def urban_search(query,client,message):
         return sendMessage(client,message,"__Error 404: not found__")
     for item in defs:
         result += "**" + item.word + "**"
-        result += "\n" + item.definition + "\n"
-        result += "__" + item.example + "__"
+        result += "\n**Definition:**\n" + item.definition + "\n"
+        result += "**Example:** __" + item.example + "__\n\n"
+        result += "++++++++++++++++++++++++++++++++\n"
     return sendMessage(client,message,result)
