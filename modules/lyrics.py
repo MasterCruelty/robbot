75b31b05feb1a1ed3d22b50357a919d87572a40c
Roberto <roberto.antoniello@studenti.unimi.it>
2022-11-02 22:01:26 +0100

    :rocket: searchsong button
    Added a button to read better albums in /searchsongs. Close #74 and continuing #15

--- modules/lyrics.py
+++ modules/lyrics.py
@@ -1,6 +1,9 @@
 import urllib.request
 from bs4 import BeautifulSoup
-from utils.get_config import sendMessage
+from utils.get_config import get_chat, get_id_msg,sendMessage
+from pyrogram import Client,filters
+from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
+from pyrogram.handlers import CallbackQueryHandler
 
 
 def execute_lyrics(query,client,message):
@@ -8,7 +11,7 @@ def execute_lyrics(query,client,message):
     if len(parametri) > 1:
         result = get_lyrics_formated(parametri[0],parametri[1])
     else:
-        result = search_songs(parametri[0])
+        return search_songs(client,message,parametri[0])
     return sendMessage(client,message,result)
 
 """
@@ -29,24 +32,68 @@ def get_lyrics_formated(artista,canzone):
     return result
 
 """
-dato l'artista, restituisce tutti i suoi brani
+dato l'artista, restituisce tutti i suoi brani.
+Le variabili globali pages e k sono usate per gestire i dati con la funzione callback per il bottone inline.
 """
-def search_songs(artista):
-    artista = format_input(artista)
-    url = "https://azlyrics.com/" + artista[0] + "/" + artista + ".html"
+global pages
+global k
+
+@Client.on_message()
+def search_songs(client,message,artista):
+    global pages
+    global k
+    artista_formated = format_input(artista)
+    url = "https://azlyrics.com/" + artista_formated[0] + "/" + artista_formated + ".html"
     page = handle_except(url)
     if "404" in str(page):
         return page
     zuppa = BeautifulSoup(page,"html.parser")
     results_tags = zuppa.find_all("div",attrs={"class": None,"id": "listAlbum"})
     results = [tag.getText() for tag in results_tags]
-    results = "__" + "\n".join(results) + "__"
-    result = ""
-    for word in results.split("\n"):
-        if word.startswith("album"):
-            word = "++++++++++++++++++++++\n**" + word + "**" + "\n"
-        result += word + "\n"
-    return result
+    results = "\n".join(results)
+    album = ""
+
+    
+    #recupero i dati e poi popolo la globale pages
+    for track in results.split("\n"):
+        if track.startswith("album"):
+            track = "{\n**" + track + "**" + "\n"
+        album += track + "\n"
+    
+    #formatto bene pages
+    pages = album.split("{")
+    pages.pop(0)
+
+
+    #costruisco la tastiera e definisco il bottone
+    kb = InlineKeyboardMarkup([[
+        InlineKeyboardButton("Prossimo Album",callback_data="next_album")]])
+
+    #aggiungo l'handler callbackquery per far catturare l'evento del bottone premuto alla funzione press_next_album
+    client.add_handler(CallbackQueryHandler(callback=press_next_album))
+
+    #mando il messaggio con la prima pagina
+    k = 0
+    client.send_message(get_chat(message),"__" + pages[k] + "__",reply_markup=kb,reply_to_message_id=get_id_msg(message))
+
+
+"""
+    Funzione che viene chiamata quando viene premuto il bottone costruito in search_songs
+    utilizza le variabili globali definite a inizio file, pages è popolata in search_songs
+    k viene incrementata in questa funzione per passare al prossimo elemento di pages.
+"""
+@Client.on_callback_query(filters.regex("next_album"))
+def press_next_album(client,message):
+    global k
+    if k < len(pages)-1:
+        k = k + 1
+        kb = InlineKeyboardMarkup([[
+            InlineKeyboardButton("Prossimo Album",callback_data="next_album")]])
+        message.edit_message_text("__" + pages[k] + "__",reply_markup=kb)
+    else:
+        message.edit_message_text("__Fine__")
+
+
 """
 Questa funzione formatta una stringa in modo che sia pronta per essere usata in get_lyrics_formated
 """
