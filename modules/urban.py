aaa6bfe293c8575a6be1ffcd21f03cfddba49840
Roberto <roberto.antoniello@studenti.unimi.it>
2022-11-02 19:01:30 +0100

    :rocket: button works
    Added CallbackQueryHandler to catch the event of the button press. Global variables used to keep the data between the functions. #15

--- modules/urban.py
+++ modules/urban.py
@@ -1,14 +1,19 @@
 import urbandictionary as ud
 from utils.get_config import * 
 from pyrogram import Client,filters
-from pyrogram.types import (InlineKeyboardButton,InlineKeyboardMarkup,CallbackQuery)
+from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup,CallbackQuery
+from pyrogram.handlers import CallbackQueryHandler
 
-
+#variabili globali indispensabili per usare i dati della prima funzione sulla seconda
+global pages
+global k
 """
     Restituisce le definizioni di urban dictionary della parola richiesta
 """
 @Client.on_message()
 def urban_search(query,client,message):
+    global pages
+    global k
     result = ""
     definitions = []
     defs = ud.define(query)
@@ -20,17 +25,29 @@ def urban_search(query,client,message):
         result += "**Example:** __" + item.example + "__\n\n"
         definitions.append(result)
     
-    i = 0
+    #I build the keyboard
     kb = InlineKeyboardMarkup([[
         InlineKeyboardButton("Next",callback_data="NEXT")] ])
-    client.send_message(get_chat(message),definitions[i],reply_markup=kb)
-    #client.edit_message_text(get_chat(message),id_msg+1,definitions[i],reply_markup=kb)
-    #press_button(client,message,kb,definitions,message.id)
-    #CallbackQuery.edit_message_text(get_chat(message),message.id+1,definitions[i+1],reply_markup=kb)
+    
+    #Uso una globale così posso usarla anche dentro press_button
+    pages = definitions
+    #Aggiungo il callback handler così che Client sappia che al premere del bottone deve richiamare la funzione passata come argomento.
+    client.add_handler(CallbackQueryHandler(callback=press_button))
+    #Mando il messaggio con la prima pagina
+    k = 0
+    client.send_message(get_chat(message),pages[k],reply_markup=kb)
 
-
-@Client.on_callback_query(filters.regex("NEXT"))
-def press_button(client,message,kb,definitions,id_msg):
-    i = 1
-    message.edit_message_text(get_chat(message),id_msg+1,definitions[i],reply_markup=kb)
-    i = i + 1
+"""
+    Funzione che viene chiamata quando il bottone costruito alla funzione urban_search viene premuto.
+    Utilizza le variabili globali di cui una popolata in urban_search e l'altra viene modificata qui.
+"""
+@Client.on_callback_query(filters = filters.regex("NEXT"))
+def press_button(client,message):
+    global k
+    if k < len(pages)-1:
+        k = k + 1
+        kb = InlineKeyboardMarkup([[
+        InlineKeyboardButton("Next",callback_data="NEXT")] ])
+        message.edit_message_text(pages[k],reply_markup=kb)
+    else:
+        message.edit_message_text("__Fine__")
