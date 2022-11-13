77e3a483ba72380a4305d92e828faedeffd94eda
Roberto <roberto.antoniello@studenti.unimi.it>
2022-11-13 17:07:37 +0100

    :recycle: Refactor help function

--- utils/sysfunctions.py
+++ utils/sysfunctions.py
@@ -40,13 +40,15 @@ def ping(query,client,message):
 """
 documentazione dei comandi utente direttamente su Telegram
 """
-#array per filtrare il comando help richiesto in help.json
-help_array = ["wiki","lyrics","covid","vaccine","poll","atm","tper","mappe","meteo","reminder","openai","urban"]
 def help(query,client,message):
     help_file = ugc.get_config_file("help.json")
-    if query in help_array:
+    if query in help_file:
         help_request = help_file[query][0:]
         help_request = str(help_request).replace("(","").replace(")","").replace('"','').replace(r'\n','\n')
+        return ugc.sendMessage(client,message,help_request)
+    elif (query not in help_file) and (query != "/helprob"):
+        help_request = "__**Comando non trovato**__\n\n"
+        help_request += help_file["default"]
         return ugc.sendMessage(client,message,help_request)
     else:
         help_request = help_file["default"]
