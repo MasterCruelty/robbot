9d5cc6538ab2b2595759302fb2dc854333ab84ae
Roberto <roberto.antoniello@studenti.unimi.it>
2022-12-21 22:41:53 +0100

    :rocket: /infopista works
    Fetch info about a specific track #102

--- modules/pistekart.py
+++ modules/pistekart.py
@@ -1,8 +1,5 @@
-from utils.get_config import sendMessage,get_chat,get_id_msg 
-from pyrogram import Client,filters,errors
-from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
-from pyrogram.handlers import CallbackQueryHandler
-import re
+from utils.get_config import sendMessage,get_chat,get_id_msg,sendPhoto
+from pyrogram import Client,errors
 import requests
 from bs4 import BeautifulSoup
 
@@ -42,4 +39,28 @@ def get_piste_region(query,client,message):
     except errors.exceptions.bad_request_400.MessageEmpty:
         return sendMessage(client,message,"__Pagina non trovata__")
 
+"""
+    restituisce informazioni dettagliate sulla pista scelta, inclusa la foto del tracciato
+"""
+def get_info_pista(query,client,message):
+    url = "https://www.pistekartitalia.it/" + query.replace(" ","-")
+    resp = requests.get(url)
+    
+    #recupero della foto del tracciato
+    zuppa = BeautifulSoup(resp.text,"html.parser")
+    images = zuppa.select('div img')
+    img_url = images[len(images)-1]['src']
+
+    #recupero descrizione pista
+    try:
+        info_track = zuppa.find_all("div",attrs={"class":"circuit-information"})
+        info_track = [tag.getText() for tag in info_track]
+        info_track = info_track[0].rsplit('Mappa',1)
+        info_track = info_track[0]
+    except IndexError:
+        info_track = "__Descrizione pista non disponibile__"
+    
+    #invio messaggio con descrizione pista e foto tracciato
+    sendMessage(client,message,"__" + info_track+ "__")
+    sendPhoto(client,message,img_url,"__Foto del tracciato__")
 
