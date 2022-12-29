0064cb61f85cb55d396f51b92b8a79db2262a7df
Roberto <roberto.antoniello@studenti.unimi.it>
2022-12-29 14:17:13 +0100

    :bug: Fix match categories

--- modules/trivial.py
+++ modules/trivial.py
@@ -11,30 +11,30 @@ import time
 from bs4 import BeautifulSoup
 
 
-categorie = {"General Knowledge"     :9,
-             "Books"                 :10,
-             "Film"                  :11,
-             "Music"                 :12,
-             "Musical & Theatres"    :13,
-             "Television"            :14,
-             "Video Games"           :15,
-             "Board Games"           :16,
-             "Science & Nature"      :17,
-             "Science & Computers"   :18,
-             "Science & Mathematics" :19,
-             "Mythology"             :20,
-             "Sports"                :21,
-             "Geography"             :22,
-             "History"               :23,
-             "Politics"              :24,
-             "Art"                   :25,
-             "Celebrities"           :26,
-             "Animals"               :27,
-             "Vehicles"              :28,
-             "Comics"                :29,
-             "Science & Gadgets"     :30,
-             "Japanese Anime & Manga":31,
-             "Cartoon & Animations"  :32}
+categorie = {"General Knowledge"                    :9,
+             "Entertainment: Books"                 :10,
+             "Entertainment: Film"                  :11,
+             "Entertainment: Music"                 :12,
+             "Entertainment: Musical & Theatres"    :13,
+             "Entertainment: Television"            :14,
+             "Entertainment: Video Games"           :15,
+             "Entertainment: Board Games"           :16,
+             "Science & Nature"                     :17,
+             "Science: Computers"                   :18,
+             "Science: Mathematics"                 :19,
+             "Mythology"                            :20,
+             "Sports"                               :21,
+             "Geography"                            :22,
+             "History"                              :23,
+             "Politics"                             :24,
+             "Art"                                  :25,
+             "Celebrities"                          :26,
+             "Animals"                              :27,
+             "Vehicles"                             :28,
+             "Entertainment: Comics"                :29,
+             "Science: Gadgets"                     :30,
+             "Entertainment: Japanese Anime & Manga":31,
+             "Entertainment: Cartoon & Animations"  :32}
 
 
 tipo_domanda = {"tf"       :"boolean",
@@ -251,14 +251,13 @@ def check_trivial_updates(client,update,users,chat):
 """
     richiamo funzione per punteggi personali
 """
-def get_personal_score(client,message):
+def get_personal_score(query,client,message):
     return personal_trivial_leaderboard(get_id_user(message),client,message)
 
 """
     richiamo funzione per classifica globale
 """
 def get_global_score(query,client,message):
-    print(query)
     for item in categorie:
         if query.title() in item:
             query = item
