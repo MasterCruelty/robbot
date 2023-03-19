68e22592ae12fbb2dac182408e4c1db95a2238b5
Roberto <roberto.antoniello@studenti.unimi.it>
2023-03-19 22:43:46 +0100

    :bug: fix revert

--- modules/ral.py
+++ modules/ral.py
@@ -13,19 +13,19 @@ import re
 """
 def ral_calc(query,client,message):
     splitted = query.split(" ")
-    ral = int(splitted[0])
+    ral = str(splitted[0])
     if not ral.isdigit():
         return sendMessage(client,message,"Errore formato.\nScopri di più con __/helprob ral__")
     if len(splitted) > 1:
-        region = splitted[1].lower()
+        region = str(splitted[1].lower())
     else:
         region = "lombardia"
-    url = "https://www.pmi.it/servizi/292472/calcolo-stipendio-netto.html?step=2&ral=" + ral + "&reg=" + region + "&com=0.8&car=no&child_noau=0&child_au=0&childh=0&childcharge=100&family=0&monthlypay=13&days=365"
+    url = "https://www.pmi.it/servizi/292472/calcolo-stipendio-netto.html?step=2&ral=" + ral + "&reg=" + region + "&com=0.8&car=no&emp=privato&hw=no&toc=ind&tow=no&child_noau=0&child_au=0&childh=0&childcharge=100&family=0&monthlypay=13&days=365"
     resp = requests.get(url)
     zuppa = BeautifulSoup(resp.text,"html.parser")
     tags = zuppa.find_all("div",attrs={"class": "income-results tbm-pdf-download"})
     results = [tag.getText() for tag in tags if tag.getText() != ""]
-    pattern = r"Stipendio netto annuo([\d.]+) €Stipendio netto mensile([\d.]+) €"
+    pattern = r"Stipendio netto complessivo([\d.]+) €Stipendio netto mensile([\d.]+) €"
     match = re.search(pattern,results[0])
     result = "Ecco la stima che desideravi:\n\n"
     if match:
