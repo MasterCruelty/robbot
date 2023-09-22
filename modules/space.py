0f892789f0961e1876f13ea4be7b09d5bf024f13
Roberto <roberto.antoniello@studenti.unimi.it>
2023-09-23 00:23:02 +0200

    :rocket: nasa apod function
    #143

---
+++ modules/space.py
@@ -0,0 +1,17 @@
+from utils.get_config import sendMessage,get_chat,get_id_msg,sendPhoto
+from pyrogram import Client,errors
+import requests
+import json
+
+"""
+    It send the daily pic from nasa APOD(Astronomy Picture of the Day)
+"""
+def get_daily_pic(query,client,message):
+    url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+    resp = requests.get(url)
+    data = json.loads(resp.text)
+    title = data['title']
+    caption = "**" + title + "**\n__" + data['explanation'] + "__"
+    pic = data['hdurl']
+    return sendPhoto(client,message,pic,caption)
+    
