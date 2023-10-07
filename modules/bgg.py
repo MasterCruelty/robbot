bb9fc2e6f7682d1e5009b9c2719aa8b24d526878
Roberto <roberto.antoniello@studenti.unimi.it>
2023-10-07 19:42:40 +0200

    :wrench: /bgg
    #137

--- modules/bgg.py
+++ modules/bgg.py
@@ -11,13 +11,10 @@ from xml.etree import cElementTree as ET
    Restituisce i dati del gioco da tavolo richiesto.
 """
 def get_board_game_data(query,client,message):
-    url_info = "https://boardgamegeek.com/xmlapi2/thing?id=" + query
+    url_info = "https://boardgamegeek.com/xmlapi2/thing?id=" + query + "&stats=1"
     data = requests.get(url_info)
 
     root = ET.fromstring(data.text)
-    result = ""
-    categories = ""
-    publisher = ""
     #prelevo titolo del gioco
     title = root.find(".//name[@type='primary']").get('value')
     #prelevo il link dell'immagine
@@ -33,21 +30,37 @@ def get_board_game_data(query,client,message):
     playingtime = root.find(".//playingtime").get('value')
     minplayingtime = root.find(".//minplaytime").get('value')
     maxplayingtime = root.find(".//maxplaytime").get('value')
-    #cerco publisher e categorie del gioco
-    #todo: boardgamemechanic, boardgameexpansion, boardgameaccessory,boardgameintegration
-    root = ET.fromstring(data.text)
+    #prelevo statistiche sul gioco da parte degli utenti su bgg
+    result = ""
+    categories = ""
+    publisher = ""
+    mecha = ""
+    access = ""
+    integr = ""
+    espan = ""
+    #cerco publisher, categorie del gioco, espansioni e integrazioni
     for link in root.iter('link'):
         link_type = link.get('type')
         if link_type == 'boardgamepublisher':
             publisher = link.get('value')
         if link_type == 'boardgamecategory':
-            categories += link.get('value') + ", "
-    caption = "**" + title + "**\n\nAnno di pubblicazione:__ " + year + "\n__Publisher:__ " + publisher + "__\nCategorie:__ " + categories + "__\n"
-    result += "__" + descr + "__\n\nNumero minimo di giocatori: __ " + minplayer + "__\n"    
-    result += "**Numero massimo di giocatori**:__ " + maxplayer + "__\n**Tempo di gioco**:__ " + playingtime + " minuti__\n**Minimo**:__ " + minplayingtime + " minuti__\n"
-    result += "**Massimo**:__ " + maxplayingtime + " minuti__"
+            categories += link.get('value') + "; "
+        if link_type == 'boardgameexpansion':
+            espan += link.get('value') + "(<code>" + link.get('id') + "</code>); "
+        if link_type == 'boardgamemechanic':
+            mecha += link.get('value') + "; "
+        if link_type == 'boardgameaccessory':
+            access += link.get('value') + "; "
+        if link_type == 'boardgameintegration':
+            integr += link.get('value') + "; "
+    caption = "**" + title + "**\n\n**Anno di pubblicazione**:__ " + year + "__\n**Publisher**:__ " + publisher + "__\n**Categorie**:__ " + categories + "__\n"
+    caption += "**Meccaniche di gioco**:__ " + mecha + "__\n**Accessori**:__" + access + "__\n**Integrazioni**:__" + integr + "__\n\n"  
+    caption += "**Numero minimo di giocatori**: __ " + minplayer + "__\n**Numero massimo di giocatori**:__" + maxplayer + "__\n"     
+    caption += "**Tempo di gioco**:__ " + playingtime + " minuti__\n**Minimo**:__ " + minplayingtime + " minuti__\n"
+    caption += "**Massimo**:__ " + maxplayingtime + " minuti__\n"
+    #result += "__" + descr + "__"    
     sendPhoto(client,message,img_url,caption)
-    return sendMessage(client,message,result)
+    #return sendMessage(client,message,result)
 
 """
     Restituisce i risultati della ricerca per la keyword inserita
