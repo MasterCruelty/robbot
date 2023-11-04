ed71978a7c61b8aed21f562d5ebc9ede03de3227
Roberto <roberto.antoniello@studenti.unimi.it>
2023-11-04 21:47:20 +0100

    :recycle: added progress messages
    #148

--- modules/videodl.py
+++ modules/videodl.py
@@ -14,6 +14,7 @@ def youtube_dl(query,client,message):
     #Scarico il video con pytube
     try:
         yt = pytube.YouTube(url)
+        sendMessage(client,message,"__Download in progress...__")
         stream = yt.streams.get_highest_resolution()
         #Lo metto in memoria senza salvarlo su disco e lo invio tramite Pyrogram
         video_bytes = io.BytesIO()
@@ -21,6 +22,7 @@ def youtube_dl(query,client,message):
     except (KeyError,pytube.exceptions.AgeRestrictedError):
         return sendMessage(client,message,"__Errore durante il download del video richiesto.\nRiprova, se il problema persiste è legato a pytube, quindi usare un altro strumento.__")
     video_bytes.seek(0)
+    sendMessage(client,message,"__Upload in progress...__")
     sendVideo(client,message,video_bytes,"__Ecco il video richiesto__")
     #clean the BytesIO file object
     video_bytes.truncate(0)
