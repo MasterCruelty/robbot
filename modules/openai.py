d623b73fd4b15cc99a44d63669c7652fc994097e
Roberto <roberto.antoniello@studenti.unimi.it>
2024-01-09 22:38:08 +0100

    removed unused variable

--- modules/openai.py
+++ modules/openai.py
@@ -14,11 +14,6 @@ Dato l'input, viene generato un testo randomico tramite le api di OpenAI
 def openai_completion(query,client,message):
     if not isSuper(get_id_user(message)) or not check_amount(get_id_user(message)):
         return sendMessage(client,message,"__Numero di utilizzi per il tuo profilo: 0\n__")
-    stop_seq = ""
-    if "stop" in query:
-        temp = query.split("/")
-        stop_seq = temp[0].replace("stop","")
-        query = temp[1]
     resp = client_openai.chat.completions.create(
     messages=[
         {
